2703fb8fdf1a82acda43114cb355dadc051dca3c
Yang <444907270@qq.com>
2019-05-06 14:12:17 +0800

    a

--- src/main/java/com/git/yang/bean/User.java
+++ src/main/java/com/git/yang/bean/User.java
@@ -3,7 +3,32 @@ package com.git.yang.bean;
 public class User {
 
 	private String name;
-	
 
-	
+	private String age;
+
+	private String pass;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 }
