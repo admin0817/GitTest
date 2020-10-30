4571431dc8faab1e0783405c5a5a45da052e5266
Yang <444907270@qq.com>
2020-10-30 12:08:09 +0800

    加点东西
    Signed-off-by: Yang <444907270@qq.com>

--- src/main/java/com/git/yang/GitBootApplication.java
+++ src/main/java/com/git/yang/GitBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitBootApplication {
-
+	// 加点东西
 	public static void main(String[] args) {
 		SpringApplication.run(GitBootApplication.class, args);
 	}
