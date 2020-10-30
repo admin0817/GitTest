60e6da156099690f7965ad43e794243579634d96
YangJiao0817 <jiaoya@vmware.com>
2020-10-30 12:30:38 +0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    这应该是一个pr
    
    Signed-off-by: YangJiao0817 <jiaoya@vmware.com>
    
    # This is the commit message #2:
    
    commit---
    
    Signed-off-by: YangJiao <jiaoya@vmware.com>

--- src/main/java/com/git/yang/GitBootApplication.java
+++ src/main/java/com/git/yang/GitBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitBootApplication {
-	// 加点东西 new
+	// 加点东西 new  prprpr  正确的:
 	public static void main(String[] args) {
 		SpringApplication.run(GitBootApplication.class, args);
 	}
