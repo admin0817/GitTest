5d4798a8356811e64018de9b20b00a416d0f7a83
YangJiao <jiaoya@vmware.com>
2020-11-06 15:29:56 +0800

    commit---
    Signed-off-by: YangJiao <jiaoya@vmware.com>

--- src/main/java/com/git/yang/GitBootApplication.java
+++ src/main/java/com/git/yang/GitBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitBootApplication {
-	// 加点东西 new  prprpr
+	// 加点东西 new  prprpr  错误的
 	public static void main(String[] args) {
 		SpringApplication.run(GitBootApplication.class, args);
 	}
